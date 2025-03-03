556292b4eb1f553572f782a9c5362ac174c75a32
Michelle Santibañez <misantibanez@microsoft.com>
2025-03-03 16:42:53 -0400

    init

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, request, jsonify
+from azure.identity import ClientSecretCredential
+import requests
+import json
+from flask import Flask, jsonify, render_template
+import os
+import logging
+
+app = Flask(__name__)
+
+# Azure Service Principal credentials
+TENANT_ID = ''
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+FABRIC_API_SCOPE = "https://analysis.windows.net/powerbi/api/.default"
+FABRIC_GRAPHQL_ENDPOINT = ''
+
+# Authenticate using the Service Principal
+credential = ClientSecretCredential(
+   tenant_id=TENANT_ID,
+   client_id=CLIENT_ID,
+   client_secret=CLIENT_SECRET
+)
+
+
+@app.route("/")
+def index():
+   return render_template('index.html')
+
+
+@app.route("/api/gql", methods=["POST"])
+def run_graphql_query():
+   """Send a GraphQL query to the Microsoft Fabric API."""
+   try:
+      # Get an access token
+      token = credential.get_token(FABRIC_API_SCOPE)
+      access_token = token.token
+
+      # Get the GraphQL query from the request
+      # Define a static GraphQL query
+      query = """        
+         query{
+            customers(first: 5) {
+               items {
+                  CustomerID
+                  FirstName
+                  LastName
+                  EmailAddress         
+               }
+            }
+            vProductAndDescriptions {
+               items {
+                  ProductID
+                  Name
+                  Description
+                  ProductModel       
+               }
+            }            
+         }
+      """
+
+
+      # Send the GraphQL query to the Fabric API
+      headers = {
+         "Authorization": f"Bearer {access_token}",
+         "Content-Type": "application/json"
+      }
+      response = requests.post(
+         FABRIC_GRAPHQL_ENDPOINT,
+         json={'query': query},
+         headers=headers
+      )
+
+      # Return the response from the Fabric API
+      return response.json(), response.status_code
+
+   except Exception as e:
+      return {"error": str(e)}, 500
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
